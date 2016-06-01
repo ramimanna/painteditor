58b407cf17d531ed6b8d1c56e79498a6a90684b5
rami13-meet <rami15manna@gmail.com>
2016-06-01 14:39:29 -0400

    freeform draw moved to easeljs, now all tools are compatible and switching between them works

--- easel.js
+++ easel.js
@@ -19,14 +19,18 @@ function Easel(){
 	/**************************************************SHAPE DRAW**************************************************/
 }
 
-Easel.prototype.lineDraw = function(s,t,shape,color="#eeeeee"){
-		shape.graphics.setStrokeStyle(3).beginStroke(color).moveTo(s[0], s[1]).lineTo(t[0], t[1]).endStroke();
+Easel.prototype.lineDraw = function(s,t,shape, color){
+		shape.graphics.setStrokeStyle(1).beginStroke(color).moveTo(s[0], s[1]).lineTo(t[0], t[1]).endStroke();		
+		//shape.alpha = 1;
 		this.stage.addChild(shape);		
+		//Bring to front
+		//this.stage.setChildIndex(shape, this.stage.getNumChildren()-1);
+
 		this.stage.update();
 		return shape;		
 }
 //While mousedown function: draws a shape
-Easel.prototype.shapeDraw = function(start,shape,container) {
+Easel.prototype.shapeDraw = function(start,shape) {
 
 		//Get shape choice and modify according to whether ShiftKey is down.
 		shape.shape_name = getChoice("Shapes");
@@ -55,8 +59,8 @@ Easel.prototype.shapeDraw = function(start,shape,container) {
 		shape.graphics.clear();
 
 		//mouse x and y distances from start point
-    	var diffx = mouse[0]-start[0];
-    	var diffy = mouse[1]-start[1];
+    	shape.diffx = mouse[0]-start[0];
+    	shape.diffy = mouse[1]-start[1];
     	//DRAW ALL SHAPES
 			//Draw Line
 			if(shape.shape_name == "Line"){
@@ -65,43 +69,46 @@ Easel.prototype.shapeDraw = function(start,shape,container) {
 			//Draw Straight Line that snaps to 90degs
 			if(shape.shape_name == "StrictLine"){
 				//Draw line at 90 degrees
-				if(Math.abs(diffy)>Math.abs(diffx)){
+				if(Math.abs(shape.diffy)>Math.abs(shape.diffx)){
 					this.lineDraw(start,[start[0],mouse[1]],shape,getChoice("Colors"));
 				}
-				else if(Math.abs(diffy)<Math.abs(diffx)){
+				else if(Math.abs(shape.diffy)<Math.abs(shape.diffx)){
 					this.lineDraw(start,[mouse[0],start[1]],shape,getChoice("Colors"));
 				}
 				//DIAGONAL: NOT WORKING FOR ALL SIDES
-				// if(Math.abs(Math.abs(diffy)-Math.abs(diffx))<=20){
-				// 	shape.graphics.lineTo(start[0]+(diffx+diffy)/2.0,start[1]+(diffx+diffy)/2.0);
+				// if(Math.abs(Math.abs(shape.diffy)-Math.abs(shape.diffx))<=20){
+				// 	shape.graphics.lineTo(start[0]+(shape.diffx+shape.diffy)/2.0,start[1]+(shape.diffx+shape.diffy)/2.0);
 				// }
 				shape.graphics.endStroke();
 			}
 	    	//Draw Oval
 	    	if(shape.shape_name == "Oval"){
-				shape.graphics/*.beginStroke("#eeeeee")*/.beginFill(getChoice("Colors")).drawEllipse(0,0,diffx,diffy);
-				shape.xradius = Math.abs(diffx);
-				shape.yradius = Math.abs(diffy);
+				shape.graphics/*.beginStroke("#eeeeee")*/.beginFill(getChoice("Colors")).drawEllipse(0,0,shape.diffx,shape.diffy);
+				shape.xradius = Math.abs(shape.diffx)/2.0;
+				shape.yradius = Math.abs(shape.diffy)/2.0;
 
 				shape.x = start[0];
 				shape.y = start[1];
 
-				shape.setBounds(shape.x-diffx,shape.y-diffy,shape.x+diffx,shape.y+diffy);
+				shape.midpoint = [shape.x+shape.xradius,shape.y+shape.yradius]
+				shape.setBounds(shape.x-shape.diffx,shape.y-shape.diffy,shape.x+shape.diffx,shape.y+shape.diffy);
 
 	    	}
 	    	//Draw Circle:
 	    	if(shape.shape_name == "Circle"){
-		    	shape.radius = Math.max(Math.abs(diffx),Math.abs(diffy))/2.0;			
+		    	shape.radius = Math.max(Math.abs(shape.diffx),Math.abs(shape.diffy))/2.0;			
 				shape.graphics/*.beginStroke("#eeeeee")*/.beginFill(getChoice("Colors")).drawCircle(0, 0, shape.radius);
 
 				//Allows drawing shapes in all directions
 				shape.x = start[0]+shape.radius;
 				shape.y = start[1]+shape.radius;
-				
-				if(diffx<0){
+
+				shape.midpoint = [shape.x,shape.y]		
+
+				if(shape.diffx<0){
 					shape.x = shape.x-2*shape.radius; 
 				}
-				if(diffy<0){
+				if(shape.diffy<0){
 					shape.y = shape.y-2*shape.radius;
 				}
 				shape.setBounds(shape.x-shape.radius,shape.y-shape.radius,shape.x+shape.radius,shape.y+shape.radius);
@@ -109,49 +116,73 @@ Easel.prototype.shapeDraw = function(start,shape,container) {
 			}  
 	    	//Draw Rectangle
 	    	else if(shape.shape_name == "Rectangle"){
-				shape.graphics/*.beginStroke("#eeeeee")*/.beginFill(getChoice("Colors")).drawRect(0,0,diffx,diffy);
+				shape.graphics/*.beginStroke("#eeeeee")*/.beginFill(getChoice("Colors")).drawRect(0,0,shape.diffx,shape.diffy);
 				shape.x = start[0];
 				shape.y = start[1];
-	    		shape.setBounds(shape.x,shape.y,diffx,diffy);
+
+				shape.midpoint = [shape.x + shape.diffx/2, shape.y + shape.diffy/2]
+	    		shape.setBounds(shape.x,shape.y,shape.diffx,shape.diffy);
 
 	    	}
 	    	//Draw Square
 	    	else if(shape.shape_name == "Square"){
-	    		if (Math.abs(diffx)>=Math.abs(diffy)){
-	   				sidex = Math.abs(diffx);
-	   				sidey = Math.abs(diffx);
+	    		if (Math.abs(shape.diffx)>=Math.abs(shape.diffy)){
+	   				sidex = Math.abs(shape.diffx);
+	   				sidey = Math.abs(shape.diffx);
 	    		}
 	    		else{
-	    			sidex = Math.abs(diffy);
-	    			sidey = Math.abs(diffy);
+	    			sidex = Math.abs(shape.diffy);
+	    			sidey = Math.abs(shape.diffy);
 	    		}
-	    		if (diffx < 0){
+	    		if (shape.diffx < 0){
 	    			sidex = -sidex;
 	    		}
-	    		if (diffy < 0){
+	    		if (shape.diffy < 0){
 	    			sidey = -sidey;
 	    		}
 	    		shape.graphics/*.beginStroke("#eeeeee")*/.beginFill(getChoice("Colors")).drawRect(0,0,sidex,sidey);
 	    		shape.x = start[0];
 	    		shape.y = start[1];
-	    		shape.setBounds(shape.x,shape.y,sidex,sidey);
-	 			container.addChild(shape);
-	    	}
+				shape.midpoint = [shape.x + sidex/2, shape.y + sidey/2]
 
+	    		shape.setBounds(shape.x,shape.y,sidex,sidey);
+	 			//container.addChild(shape);
+	    	}
+	    //Add Shadow	
+		//shape.shadow = new createjs.Shadow('#000', 4, 4, 5);
+		//console.log("shape.midpoint",shape.midpoint);
     	this.current_shape = shape;
-    	this.current_shape_container = container;
+    	//this.current_shape_container = container;
 		shape.selection_lines = [];
 
 		this.stage.addChild(shape);
 		this.stage.update();
 }
-Easel.prototype.dragOn = function(evt){
+
+Easel.prototype.rotate = function(start){
+	//Calculate delta-angle of mouse from midpoint, from dot product of vectors: (center->start) DOT (center->mouse)
+	//this.selected_shape.rotation++;
+	v1 = subtract_vec(start,this.selected_shape.midpoint);
+	v2 = subtract_vec(mouse,this.selected_shape.midpoint);
+	angle = dot(v1,v2)/(magnitude(v1)*magnitude(v2));
+
+	this.stage.update;
+}
+
+Easel.prototype.resize = function(){
+
+}
+
+Easel.prototype.dragOn = function(){
 	   	if(mouse != this.start){
+	   		x_change = mouse[0] - this.start[0];
+	   		y_change = mouse[1] - this.start[1];
 	   		//Move to next location
-		   	this.selected_shape.x = this.selected_shape_start[0] + mouse[0] - this.start[0]; //REPLACE MOUSE WITH MORE ACCURATE VERSION?
-		    this.selected_shape.y = this.selected_shape_start[1] + mouse[1] - this.start[1];
-		    //Bring to front
-			this.stage.setChildIndex(this.selected_shape, this.stage.getNumChildren()-1);
+		   	this.selected_shape.x = this.selected_shape_start[0] + x_change;
+		    this.selected_shape.y = this.selected_shape_start[1] + y_change;
+		    // this.selected_shape.midpoint = [this.selected_shape.midpoint[0]+x_change,this.selected_shape.midpoint[1]+y_change];
+		 //    //Bring to front
+			// this.stage.setChildIndex(this.selected_shape, this.stage.getNumChildren()-1);
 		    if(this.mouseIsDown){
 		    	this.select();
 		    }
@@ -169,31 +200,67 @@ Easel.prototype.select = function(){
 		left_x = this.selected_shape.x;
 		right_x = this.selected_shape.x + this.selected_shape.bounds.width;
 		top_y = this.selected_shape.y;
-		bottom_y = this.selected_shape.y + this.selected_shape.bounds.height;		
+		bottom_y = this.selected_shape.y + this.selected_shape.bounds.height;
+		this.selected_shape.midpoint = [this.selected_shape.x + this.selected_shape.diffx/2, this.selected_shape.y + this.selected_shape.diffy/2]; //STILL HAVE TO DO FOR SQUARES!
+
 	}
 	else if(this.selected_shape.shape_name == "Circle"){
-		console.log("Caught circle");
 		left_x = this.selected_shape.x-this.selected_shape.radius;
 		right_x = this.selected_shape.x + this.selected_shape.radius;
 		top_y = this.selected_shape.y-this.selected_shape.radius;
-		bottom_y = this.selected_shape.y + this.selected_shape.radius;	
+		bottom_y = this.selected_shape.y + this.selected_shape.radius;
+		this.selected_shape.midpoint = [this.selected_shape.x,this.selected_shape.y];
+
 	}
 	else if(this.selected_shape.shape_name == "Oval"){
 		left_x = this.selected_shape.x;
-		right_x = this.selected_shape.x + this.selected_shape.xradius;
+		right_x = this.selected_shape.x + 2*this.selected_shape.xradius;
 		top_y = this.selected_shape.y;
-		bottom_y = this.selected_shape.y + this.selected_shape.yradius;			
+		bottom_y = this.selected_shape.y + 2*this.selected_shape.yradius;
+		this.selected_shape.midpoint = [this.selected_shape.x+this.selected_shape.xradius,this.selected_shape.y+this.selected_shape.yradius];		
 	}
+	left_x -=.5
+	right_x +=.5
+	top_y -=.5
+	bottom_y +=.5
+
+
 
 	points = [[left_x,top_y],[right_x,top_y],[right_x,bottom_y],[left_x,bottom_y]];
-	//console.log(points);
+	side_mids = [[this.selected_shape.midpoint[0],top_y],[this.selected_shape.midpoint[0],bottom_y],[left_x,this.selected_shape.midpoint[1]],[right_x,this.selected_shape.midpoint[1]]];
 	for (i = 0; i < 4; i++) {
-		console.log(i,i+1);
-		line = this.lineDraw(points[i%4],points[(i+1)%4],this.shape);
+		//DRAW LINES:
+		line = this.lineDraw(points[i%4],points[(i+1)%4],this.shape,"#000000");
 		this.selected_shape.selection_lines.push(line);
 	}
-	//Bring to front
-	this.stage.setChildIndex(this.selected_shape, this.stage.getNumChildren()-1);
+	for(i=0; i <4; i++){
+		//DRAW SIDE CIRCLES:
+		side_circle = new createjs.Shape();
+		side_circle.radius = 3.5;
+		side_circle.graphics.setStrokeStyle(1).beginStroke("#000000").beginFill("#ffffff").drawCircle(0, 0, side_circle.radius);
+
+		//put in each corner
+		side_circle.x = side_mids[i][0];
+		side_circle.y = side_mids[i][1];
+		this.selected_shape.side_circles.push(side_circle);
+		this.stage.addChild(side_circle);
+
+		//DRAW CORNER CIRCLES:
+		corner_circle = new createjs.Shape();
+		corner_circle.radius = 3.5;
+		corner_circle.graphics.setStrokeStyle(1).beginStroke("#000000").beginFill("#ffffff").drawCircle(0, 0, corner_circle.radius);
+
+		//put in each corner
+		corner_circle.x = points[i][0];
+		corner_circle.y = points[i][1];
+		this.selected_shape.corner_circles.push(corner_circle);
+		this.stage.addChild(corner_circle);
+	}
+
+	for(i=0;i<45;i++){
+		//this.rotate();
+	}
+
 	this.stage.update();
 }
 
@@ -201,28 +268,54 @@ Easel.prototype.deselect = function(){
 	//clear last update of shape
 	for(i = 0; i < this.selected_shape.selection_lines.length; i++){
 		line = this.selected_shape.selection_lines[i];
-		console.log("line",line);
 	 	line.graphics.clear();
+	 	side_circle = this.selected_shape.side_circles[i];
+	 	side_circle.graphics.clear();
+	 	corner_circle = this.selected_shape.corner_circles[i];
+	 	corner_circle.graphics.clear();
 	}
+
 	this.stage.update();
 	this.selected_shape.selection_lines=[];
+	this.selected_shape.corner_circles = [];
+	this.selected_shape.side_circles = [];
 }
+
+//While mousedown function: draws a line as the user clicks and moves the mouse
+Easel.prototype.draw = function(start,shape) {
+	this.lineDraw(start,mouse,shape,getChoice("Colors"));
+}
+
 //MOUSE CONTROLS:
 Easel.prototype.mouseDown = function(){
     this.mouseIsDown = true;
 	this.start = mouse;
 	if(current_tool=="shapeDraw"){
 	    this.shape = new createjs.Shape();
-    	this.container = new createjs.Container();
+    	//this.container = new createjs.Container();
+	}
+	else if(current_tool == "draw"){
+		this.shape = new createjs.Shape();
+		//this.shape.graphics.setStrokeStyle(3, "round").moveTo(this.start[0],this.start[1]);
 	}
 
 	else if(current_tool=="cursor"){
 		//If another shape is selected, deselect it
+		this.prev_selected_shape = this.selected_shape;
 		if(this.selected_shape){
 			this.deselect();
 		}
 		//set the new shape to select
 		this.selected_shape = this.stage.getObjectUnderPoint(mouse[0],mouse[1]);
+		//console.log(this.prev_selected_shape,($.inArray(this.prev_selected_shape,this.shapes)!= -1));
+		
+		// if(this.prev_selected_shape && !this.selected_shape && ($.inArray(this.prev_selected_shape,this.shapes)!= -1)){
+		// 	this.selected_shape = this.prev_selected_shape;
+		// 	this.select();
+		// 	console.log("YASS!");
+		// 	console.log("selected:",this.selected_shape);
+		// }
+
 		//if it is a shape and not null:
 		if(this.selected_shape){
 			//SELECT THE SELECTED SHAPE!
@@ -238,27 +331,48 @@ Easel.prototype.mouseUp = function(){
 	this.mouseIsDown = false;
 	if(current_tool == "shapeDraw"){
 		this.shapes.push(this.current_shape);
-		this.shape_containers.push(this.current_shape_container);
+		//this.shape_containers.push(this.current_shape_container);
 	}
 }
 Easel.prototype.mouseMove = function(evt){
 	if(current_tool=="shapeDraw"){
 		if (this.mouseIsDown){
-			this.shapeDraw(this.start,this.shape,this.container);
+			this.shapeDraw(this.start,this.shape);//,this.container);
+		}
+	}
+
+	if(current_tool == "draw"){
+		if (this.mouseIsDown){
+			this.draw(this.start, this.shape);
+			this.start = mouse;
 		}
 	}
 	else if(current_tool=="cursor"){
 		if(this.selected_shape && this.mouseIsDown){
 				this.dragOn(evt);
 		}
+
 	}
 }
+
+Easel.prototype.doubleClick = function(){
+    //Bring to front
+	this.stage.setChildIndex(this.selected_shape, this.stage.getNumChildren()-1);
+	this.stage.update();
+}
+
 
 //TOOL TOGGLE FUNCTIONS:
 Easel.prototype.cursorTool = function(){
 	current_tool = "cursor";
 	console.log(current_tool);
 }
+
+Easel.prototype.drawTool = function(){
+	current_tool = "draw";
+	console.log(current_tool);
+}
+
 Easel.prototype.shapeDrawTool = function(){
 	current_tool = "shapeDraw";
 	console.log(current_tool);
--- painteditor.js
+++ painteditor.js
@@ -2,69 +2,50 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var colors = {"Blue":"#1976D2","Purple":"#673AB7","Green":"#009688","Red":"#D32F2F","Orange":"#FF5722","Black":"#000000","White":"#ffffff"}
 var current_tool = null;
-//Keep Track of mouse position in variable: mouse
+
+
+//Keep track of these mouse (mouse position) and shiftDown (if shift key is down) with a listener when the mouse moves
 var mouse=[0,0];
-//Keep Track of whether the shift key is down (only need to do this while mouse is moving)
-var shiftDown = false;
-//Keep track of these 2 things with a listener when the mouse moves
+var shiftDown = false; //this only tracks shift key when mouse is moving
 document.addEventListener('mousemove', function(e) {
   mouse = [e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop];
+  shiftDown = false;
   if(event.shiftKey){
   	shiftDown = true;
   }
-  else{
-  	shiftDown = false;
-  }
 });
 
-//While mousedown function: draws a line as the user clicks and moves the mouse
-function draw() {
-	ctx.lineWidth = 3;
-	ctx.lineJoin = 'round';
-	ctx.lineCap = 'round';
-
-    ctx.strokeStyle = colors[getChoice("Colors")];
-    ctx.lineTo(mouse[0], mouse[1]);
-    ctx.stroke();
+//Vector Math Helper Functions
+function dot(a,b){
+  if (a.length != b.length){
+    throw "dot of different length vectors";	
+  }
+  result = 0;
+  for(i=0;i<a.length;i++){
+    result += a[i]*b[i];	
+  }
+  return result;
 }
 
-//While DrawOn(), Functions for when mouse is up and down
-function drawOnMouseUp() {
-    canvas.removeEventListener('mousemove', draw);
-}
-function drawOnMouseDown() {
-    ctx.moveTo(mouse[0], mouse[1]);
-    ctx.beginPath();
-    canvas.addEventListener('mousemove', draw);
-}
-
-//Turns on drawing (freeform)
-function drawOn(){
-	canvas.addEventListener('mousedown', drawOnMouseDown);
-	canvas.addEventListener('mouseup', drawOnMouseUp);
-	canvas.addEventListener("mouseout", drawOnMouseUp);
+function subtract_vec(v1,v2){
+  if (v1.length != v2.length){
+    throw "subtract_vec of different length vectors";
+  }
+  result = [];
+  for(i=0;i<v1.length;i++){
+    result.push(v1[i]-v2[i]);  	
+  }
+  return result;
 }
 
-//Turns off drawing (freeform)
-function drawOff(){
-	console.log("drawOff");
-	canvas.removeEventListener('mousemove', draw);
-	canvas.removeEventListener('mousedown', drawOnMouseDown);
-	canvas.removeEventListener('mouseup', drawOnMouseUp);
-	canvas.removeEventListener("mouseout", drawOnMouseUp);
-}
-
-//Uses drawOff() and drawOn() to toggle freeform
-function toggleDraw(){
-	if($("#myonoffswitch").prop('checked') == true){
-	    drawOn();
+function magnitude(v1){
+	result = 0;
+	for(i=0;i<v1.length;i++){
+		result+=v1[i]*v1[i];
 	}
-	else{
-		drawOff();
-	}	
+	return Math.sqrt(result);
 }
-
-//Takes in topic ("Colors","Shapes",) and returns the current choice from its respective dropdown menue
+//Returns selected choice from dropdown lists with topic="Colors","Shapes"
 function getChoice(topic){
 	var choice = document.getElementById(topic)[document.getElementById(topic).selectedIndex].value;
 	return choice;
@@ -75,11 +56,23 @@ var current_tool = "cursor";
 canvas.addEventListener("mousedown",myEasel.mouseDown.bind(myEasel));
 canvas.addEventListener("mouseup",myEasel.mouseUp.bind(myEasel));
 canvas.addEventListener("mousemove",myEasel.mouseMove.bind(myEasel));
+canvas.addEventListener("dblclick",myEasel.doubleClick.bind(myEasel));
+
 // myEasel.stage.on("pressmove",myEasel.pressMove.bind(myEasel));
+
+// myEasel.stage.on("stagemousedown", function(evt) {
+// 	this.stage.on("stagemouseup", stage_mouse_up = function(evt){
+// 	    alert("the canvas was clicked at "+evt.stageX+","+evt.stageY);
+// 	    this.stage.off("stagemouseup",stagemouseup);
+// 	});
+// });
 
 $('#cursorTool').click(function(){
 	myEasel.cursorTool();
 });
 $('#shapeDrawTool').click(function(){
 	myEasel.shapeDrawTool();	
+});
+$('#drawTool').click(function(){
+	myEasel.drawTool();	
 });
